7a668557b669846290d375ad3c597cfa79cb7a92
rsookram <rashadsookram@gmail.com>
2022-01-07 19:56:28 -0500

    Fix display of widget

--- app/src/main/java/io/github/rsookram/soon/glance/SoonAppWidgetReceiver.kt
+++ app/src/main/java/io/github/rsookram/soon/glance/SoonAppWidgetReceiver.kt
@@ -1,8 +1,8 @@
 package io.github.rsookram.soon.glance
 
+import android.appwidget.AppWidgetManager
 import android.content.Context
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.unit.dp
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
@@ -22,9 +22,12 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.EntryPointAccessors
 import dagger.hilt.components.SingletonComponent
+import io.github.rsookram.soon.ApplicationScope
 import io.github.rsookram.soon.Todo
 import io.github.rsookram.soon.data.Repository
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 import javax.inject.Provider
@@ -33,9 +36,25 @@ import javax.inject.Provider
 class SoonAppWidgetReceiver : GlanceAppWidgetReceiver() {
 
     @Inject lateinit var widgetFactory: Provider<SoonWidget>
+    @Inject lateinit var repository: Repository
+    @Inject @ApplicationScope
+    lateinit var scope: CoroutineScope
 
     override val glanceAppWidget: GlanceAppWidget
         get() = widgetFactory.get()
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+
+        // Handle updating the tasks displayed for the day when this is called on a new day.
+        scope.launch {
+            repository.refreshAgenda()
+        }
+    }
 }
 
 private val toggledTodoIdKey = ActionParameters.Key<ByteArray>("ToggledTodoIdKey")
@@ -45,11 +64,6 @@ class SoonWidget @Inject constructor(private val repository: Repository) : Glanc
     @Composable
     override fun Content() {
         val agenda = runBlocking { repository.agenda.first() }
-
-        // Handle updating the tasks displayed for the day when this is called on a new day.
-        LaunchedEffect(Unit) {
-            repository.refreshAgenda()
-        }
 
         LazyColumn(GlanceModifier.fillMaxSize().padding(16.dp)) {
             items(agenda.todos) { todo ->
